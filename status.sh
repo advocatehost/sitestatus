997ad529b719fb0ad35a7bf273b77ba2abc8f519
Ezekiel Hammond <ezekiel.contractor@advocatehost.com>
2017-11-19 01:38:17 +0000

    Initial push

--- status.sh
+++ status.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+<<alias
+if ! grep -q sniff "$HOME/.bash_profile"; then
+
 echo "alias sniff='curl -w \"@/path/to/sniff.txt\" -o /dev/null -O --remote-name --remote-header-name -s'" >> ~/.bash_profile
 	source  ~/.bash_profile
 
-
+fi
+alias
+alias sniff='curl -w "./sniff.txt" -o /dev/null -O --remote-name --remote-header-name -s'
+sniff google.com
